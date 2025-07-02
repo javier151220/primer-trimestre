d499ab374fd15110bbfca1a7e64d207571adda4b
javier solis <jsruiz932@gmail.com>
2025-07-02 17:32:37 -0500

    sena

--- Taller_de_aplicaciones_tuplas listas y diccionarios.py
+++ Taller_de_aplicaciones_tuplas listas y diccionarios.py
@@ -100,8 +100,218 @@
 # print(lista)
 # print(f"la suma total es de {suma}")
 
+#inventario con lista de diccionario 7
+# lista=[]
+# pro1={ 
 
-#INVENTARIO CON LISTA DE DICCIONARIO
+#     "productos": input("ingrese el nombre del producto"),
+#     "cantidad": int(input("cuantas unidades : ")),
+#    " precio": float(input("ingresa el precio"))
+# }
+# lista.append(pro1)
+# print(lista)
+
+# pro2={ 
+
+#     "productos": input("ingrese el nombre del producto"),
+#     "cantidad": int(input("cuantas unidades : ")),
+#    " precio": float(input("ingresa el precio"))
+# }
+# lista.append(pro2)
+# print(lista)
+
+
+# pro3={ 
+
+#     "productos": input("ingrese el nombre del producto"),
+#     "cantidad": int(input("cuantas unidades : ")),
+#    " precio": float(input("ingresa el precio"))
+# }
+# lista.append(pro3)
+# print(lista)
 
 
 
+
+
+#MOdificar una lista de precios 8
+
+# lista=[]
+# print(lista)
+# precio1=int(input("ingresa un precio : "))
+# precio2=int(input("ingresa un precio : "))
+# precio3=int(input("ingresa un precio : "))
+# precio4=int(input("ingresa un precio : "))
+# precio5=int(input("ingresa un precio : "))
+# lista.append(precio1)
+# lista.append(precio2)
+# lista.append(precio3)
+# lista.append(precio4)
+# lista.append(precio5)
+# print(lista)
+# descuento=
+
+
+#Notas con tupla 9
+
+# tupla=()
+# print(tupla)
+# lista=[]
+# nota1=float(input("ingresa una nota"))
+# nota2=float(input("ingresa una nota"))
+# nota3=float(input("ingresa una nota"))
+# lista=list(tupla)
+# lista.append(nota1)
+# lista.append(nota2)
+# lista.append(nota3)
+# tupla=tuple(lista)
+# print(tupla)
+# print(lista)
+# print(f"la nota mas alta es {max(tupla)} y la nota mas baja fue {min(tupla)}")
+
+
+#Diccionario de conversiones 10
+
+# conversiones = {
+#     "kilometros": 1000, 
+#     "metros": 1, 
+#     "centimetros": 0.01
+#     }
+# cantidad = 5
+# unidad = "km"
+# metros = cantidad * conversiones[unidad]
+# print(f"la cantidad  es {cantidad}, la unidad es {unidad} y los {metros} metros")
+
+
+#Diccionario de conversiones 11
+ 
+# precios=[]
+# precio1=float(input("ingresa un precio : "))
+# precio2=float(input("ingresa un precio : "))
+# precio3=float(input("ingresa un precio : "))
+# precios.append(precio1)
+# precios.append(precio2)
+# precios.append(precio3)
+# print(precios)
+
+# precio2=[]
+
+# aumento1=precios[0]*1.19
+# aumento2=precios[1]*1.19
+# aumento3=precios[2]*1.19
+
+# precio2.append(aumento1)
+# precio2.append(aumento2)
+# precio2.append(aumento3)
+# print(precio2)
+
+
+
+#Tupla de operaciones matemáticas 12
+
+# tupla=()
+# print(tupla)
+# num1=int(input("ingresa un numero"))
+# num2=int(input("ingresa un numero"))
+# lista=list(tupla)
+# lista.append(num1)
+# lista.append(num2)
+# print(lista)
+# operacion1=(lista[0]+lista[1])
+# operacion2=(lista[0]-lista[1])
+# operacion3=(lista[0]*lista[1])
+# operacion4=(lista[0]/lista[1])
+# print(operacion1)
+# print(operacion2)
+# print(operacion3)
+# print(operacion4)
+# lista.append(operacion1)
+# lista.append(operacion2)
+# lista.append(operacion3)
+# lista.append(operacion4)
+# print(lista)
+
+#Diccionario de estudiantes 13
+
+# notas = {"sergio": 1,
+#         "tamallo": 3,
+#         "solis": 4, 
+#         "Aily": 2
+# }
+# print(notas)
+# promedio = (notas["sergio"] + notas["tamallo"] + notas["solis"] + notas["Aily"]) / 4
+# print(f"en generar el promedio es {promedio}")
+
+
+
+
+#Lista de salarios 14
+
+# salario1=[]
+# print(salario1)
+
+# sal1=float(input("ingrese un salario"))
+# sal2=float(input("ingrese un salario"))
+# sal3=float(input("ingrese un salario"))
+# sal4=float(input("ingrese un salario"))
+# sal5=float(input("ingrese un salario"))
+
+# salario1.append(sal1)
+# salario1.append(sal2)
+# salario1.append(sal3)
+# salario1.append(sal4)
+# salario1.append(sal5)
+
+# print(salario1)
+
+# salario2=[]
+
+# aumento1=salario1[0]*1.10
+# aumento2=salario1[1]*1.10
+# aumento3=salario1[2]*1.10
+# aumento4=salario1[3]*1.10
+# aumento5=salario1[4]*1.10
+# salario2.append(aumento1)
+# salario2.append(aumento2)
+# salario2.append(aumento3)
+# salario2.append(aumento4)
+# salario2.append(aumento5)
+# print(salario2)
+
+
+
+#Diccionario de impuestos 15
+
+# frutas= {"pera": 1.0, 
+# "manzana": 1.5, 
+#  "uva": 2.0
+# }
+# impuesto = 0.21  # 21%
+# pera = frutas["pera"] * (1 + impuesto)
+# manzana= frutas["manzana"] * (1 + impuesto)
+# uva = frutas["uva"] * (1 + impuesto)
+# print(f"la fruta pera: {pera}, la manzana: {manzana}, y la uva uva: {uva}")
+
+#nálisis de lista de edades:16
+
+# edades = [11, 33, 90, 45, 50]
+# mayores = (edades[0] >= 18) + (edades[1] >= 18) + (edades[2] >= 18) + (edades[3] >= 18) + (edades[4] >= 18)
+# menores = 5 - mayores
+# print(f"los mayores de edad son : {mayores}, y los menore de edad son: {menores}")
+
+
+#Tupla de conversiones de moneda 17
+# dolar = 100
+# euro = 0.85
+# peso = 17.0
+# yen = 110.0
+# conversiones = (dolar* euro, dolar * peso, dolar * yen)
+# print(f"Euros: {conversiones[0]},  en pesos Pesos: {conversiones[1]},  y en Yenes: {conversiones[2]}")
+
+#Diccionario de ventas 18
+
+
+#Lista de temperaturas extremas 19
+
+
+#Actualizar precios con métodos de listas 20
