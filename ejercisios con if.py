08556f0f473703705dc535263be43d2d9c7dc894
javier solis <jsruiz932@gmail.com>
2025-07-09 17:39:27 -0500

    SENA

---
+++ ejercisios con if.py
@@ -0,0 +1,111 @@
+#javier usando if
+
+
+# 1 verifica si un numero es positivi negativo o cero
+
+# numro=int(input("ingresa un numero : "))
+# if numro >0:
+#     print(f"es positivo, {numro}")
+# elif numro<0:
+#     print(f"es negativo, {numro}")
+# else:
+#     print(f"es zero, {numro}")
+
+
+# 2  CALCULA EL MAYOR DE DOS NUMEROS INGRESADOS
+
+# num1=int(input("ingresa un numero : "))
+# num2=int(input("ingresa un numero : "))
+# if num1>num2 :
+#     print(f"el primer numero es es mayor")
+# else:
+#     print(f"el segundo numero  es mayor")
+
+#numero3
+
+#----3 determine si un numero es par o impar-------
+
+# numero=int(input("ingresa un numero:"))
+# if numero %2 == 0:
+#     print("es par")
+# else:
+#     print("no es par")
+
+
+# -4 DADO UN NUMERO VERIFIQUE SI ESTA DENTRO DE 10 Y 20
+
+# NUMERO=int(input("ingrese un  numero"))
+
+# if NUMERO>=10 and NUMERO<=20:
+#     print("esta dentro del rango")
+# else:
+#     print("no esta dentro del rango")
+
+
+# 5  DADO TRES NUMEROS  ENCUENTRA CUAL ES MAYOR  USANDO CONDICIONALES
+
+# numero_1=int(input("ingresa un numero :"))
+# numero_2=int(input("ingresa un numero :"))
+# numero_3=int(input("ingresa un numero :"))
+
+# if numero_1>numero_2 and numero_1>numero_3:
+#     print(f"este numero es mayor {numero_1}")
+# elif numero_2>numero_1 and numero_2>numero_3:
+#     print(f"este numero es mayor {numero_2}")
+# elif numero_3>numero_1 and numero_3>numero_2:
+#     print(f"este numero rs mayor {numero_3}")
+# else:
+#     print(f"todos son guales {numero_1}, {numero_2}, {numero_3}")
+
+
+# 6 calcule el precio final con un 10%  si el total es mayor a 100
+
+
+# fruta=input("Ingresa una fruta :")
+# pre=float(input("Ingresa el precio de la fruta"))
+# if pre>100:
+#     operacion=pre*0.10
+#     print(f"el descuento es {operacion}" )
+# else:
+#     print("no hay descuento")
+
+# 7 verifica si una persona puede votar (mayor o ihual a 18 años)
+
+# nombre=input("ingresa tu nombre : ")
+# edad=int(input("ingresa tu edad :"))
+# if edad>=18:
+#     print(f"puedes votar porque tienes {edad}")
+# else:
+#     print(f"eres menor de edad porque tienes {edad}")
+
+# 8 dado el precio y tipo de cliente (VIP o normal),aplica un descuento del 20% solo a vip
+
+# producto=input("Ingresa un profucto :")
+# precio=float(input("Ingresa el precio :"))
+# cliente=input("Que tipo de cliente eres VIP o normal :")
+# if cliente=="VIP":
+#     des=precio*20
+#     print(f"con el descuento te queeda en  {des}")
+# else:
+#     print("no tienes descuento")
+
+#9 determina si un numero es multiplo de 5 y de 3 al mismo tiempo
+
+# numero=int(input("ingresa un numero:"))
+# if numero % 3== 0 and numero % 5 == 0:
+#     print("es multiplo de 3 y 5")
+# else:
+#     print("no es multiplo")
+
+#10 dado un numero verifica si es divisible entre dos numero dados
+
+# numero1=int(input("ingresa un numero : "))
+# numero2=int(input("ingresa un numero : "))
+# numero3=int(input("ingresa un numero : "))
+# if numero1 % numero2  % numero3 == 0:
+#     print(f"Si es divisible")
+# else:
+#     print("no es divisible")
+    
+
+
